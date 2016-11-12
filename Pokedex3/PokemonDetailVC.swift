a295196423d2ac95d9df3aa96684ca09cbcba599
Elvis <powerdrift.chen@gmail.com>
2016-11-12 22:55:29 +0800

    Add PokemonDetailViewController and Segue

---
+++ Pokedex3/PokemonDetailVC.swift
@@ -0,0 +1,25 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex3
+//
+//  Created by Elvis Chen on 12/11/2016.
+//  Copyright © 2016 Elvis Chen. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    var pokemon: Pokemon!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+
+    }
+
+
+}
