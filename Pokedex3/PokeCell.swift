7ee9d64f5c7c49797e0cf14331cdf652f20531f4
Elvis <powerdrift.chen@gmail.com>
2016-11-12 20:23:30 +0800

    Setup UICollectionViewCell

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Elvis Chen on 12/11/2016.
+//  Copyright © 2016 Elvis Chen. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+}
