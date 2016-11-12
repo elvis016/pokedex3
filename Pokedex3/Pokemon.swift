bb50d9963a0210a266e0344a26cc697b6456e8df
Elvis <powerdrift.chen@gmail.com>
2016-11-12 16:46:02 +0800

    Added Pokemon model and setup collection and search bar

---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,35 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Elvis Chen on 12/11/2016.
+//  Copyright © 2016 Elvis Chen. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+    
+    
+    
+}
